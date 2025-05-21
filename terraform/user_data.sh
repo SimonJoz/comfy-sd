22836967f09b039032524bad9afeaeacbbe832a0
sjozefczuk <szjozefczuk@pgs-soft.com>
2025-05-21 20:39:18 +0200

    workflows added

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -19,7 +19,7 @@ libgl1-mesa-glx libglib2.0-0 libsm6 libxrender1 libxext6
 # ======================================================================================
 
 pip install --upgrade pip
-python3 -m venv comfy_env
+python -m venv comfy_env
 source comfy_env/bin/activate
 
 pip install comfy-cli
@@ -45,9 +45,9 @@ hugging_face_models["sd_xl_refiner_1.0.safetensors"]="https://huggingface.co/sta
 hugging_face_models["flux1-schnell-fp8.safetensors"]="https://huggingface.co/Comfy-Org/flux1-schnell/resolve/main/flux1-schnell-fp8.safetensors"
 hugging_face_models["flux1-dev-fp8.safetensors"]="https://huggingface.co/Comfy-Org/flux1-dev/resolve/main/flux1-dev-fp8.safetensors"
 
-#for key in "${!hugging_face_models[@]}"; do
-#  comfy model download --relative-path "$CHECKPOINTS_DIR" --filename "$key" --url "${hugging_face_models[$key]}"
-#done
+for key in "${!hugging_face_models[@]}"; do
+  comfy model download --relative-path "$CHECKPOINTS_DIR" --filename "$key" --url "${hugging_face_models[$key]}"
+done
 
 # FLUX
 comfy model download --relative-path ./models/unet/ --filename flux1-dev.safetensors \
@@ -173,25 +173,53 @@ comfy model download --relative-path ./models/animatediff_models --filename v3_s
 # ======================================================================================
 
 custom_nodes=(
-  "rgthree-comfy"
-  "comfyui_ultimatesdupscale"
-  "comfyui-animatediff-evolved"
-  "comfyui_instantid"
-  "comfyui-reactor"
-  "comfyui_controlnet_aux"
-  "cg-use-everywhere"
-  "x-flux-comfyui"
-  "comfyui_essentials"
-  "comfyui-impact-pack"
-  "comfyui-inspire-pack"
-  "comfyui-florence2"
-  "was-node-suite-comfyui"
-  "ComfyUI-KJNodes"
-  "comfyui-videohelpersuite"
-
-# ComfyUI_IPAdapter_plus/
-# ComfyUI-InstantID/
-# ComfyUI-DeepFuze/
+#"was-node-suite-comfyui"                # WAS Node Suite
+#"comfyui_ultimatesdupscale"             # ComfyUI_UltimateSDUpscale
+#"comfyui-inspire-pack"                  # ComfyUI Inspire Pack
+#"comfyui_controlnet_aux"                # comfyui_controlnet_aux
+#"comfyui-animatediff-evolved"           # ComfyUI-AnimateDiff-Evolved
+#"cg-use-everywhere"                     # cg-use-everywhere
+#"comfyui-videohelpersuite"              # ComfyUI-VideoHelperSuite
+#"comfyui-frame-interpolation"           # ComfyUI-Frame-Interpolation
+#"comfyui_instantid"                     # ComfyUI_InstantID
+#"comfyui-hunyuanvideowrapper"           # ComfyUI-HunyuanVideoWrapper
+#"comfyui-custom-scripts"                # ComfyUI-Custom-Scripts
+#"comfyui-florence2"                     # ComfyUI-Florence2
+#"comfyui-advanced-controlnet"           # ComfyUI-Advanced-ControlNet
+#"comfyui-mxtoolkit"                     # ComfyUI-mxToolkit
+#"comfyui-image-saver"                   # ComfyUI Image Saver
+#"ComfyUI-GGUF"                          # ComfyUI-GGUF
+#"comfyui-kjnodes"                       # ComfyUI-KJNodes
+#"rgthree-comfy"                         # rgthree-comfy
+#"comfyui-impact-pack"                   # ComfyUI Impact Pack
+#"gguf"                                  # gguf
+#"comfyui_essentials"                    # ComfyUI_essentials
+#"x-flux-comfyui"                        # x-flux-comfyui
+#"ComfyUI-GGUF_Forked"                   # ComfyUI-GGUF_Forked
+#"comfyui-florence2xy"                   # comfyui-florence2xy
+#"comfyui-reactor-node"                  # comfyui-reactor-node
+#"comfyui-logicutils"                    # ComfyUI-LogicUtils
+#"comfyui-get-meta"                      # comfyui-get-meta
+#"comfyui-reactor"                       # ComfyUI-ReActor
+#"teacachehunyuanvideo"                  # ComfyUI-TeaCacheHunyuanVideo
+#"ComfyUI-Apt_Preset"                    # ComfyUI-Apt_Preset
+#"ComfyUI-InstantIDUtils"                # ComfyUI-InstantIDUtils
+#"agilly1989_motorway"                   # ComfyUI_agilly1989_motorway
+#"ComfyUI-InstantID"                     # ComfyUI-InstantID
+#"ComfyUI-DeepFuze"                      # DeepFuze
+#"comfyui_essentials_mb"                 # ComfyUI_essentials_mb
+#"comfyui-web-viewer"                    # ComfyUI Web Viewer
+#"comfyui-clh-tool"                      # ComfyUi-clh-Tool
+#"ComfyUI-FramePackWrapper_Plus"         # ComfyUI-FramePackWrapper_Plus
+#"ComfyUI_Comfyroll_CustomNodes"         # Comfyroll Studio
+#"comfystereo"                           # ComfyStereo
+#"comfy-image-saver"                     # Save Image with Generation Metadata
+#"comfyui-tensorops"                     # comfyui-tensorop
+#"ComfyUI-JNodes"                        # ComfyUI-JNodes
+#"ComfyLiterals"                         # ComfyLiterals
+#"teacache"                              # ComfyUI-TeaCache
+#"comfyui-detail-daemon"                 # ComfyUI-Detail-Daemon
+#"comfyui-multigpu"                      # ComfyUI-MultiGPU
 )
 
 for cn in "${custom_nodes[@]}";
